343f701fd72eebde6ddfede55f44c9389d81ef2f
nero-caesar <nzehoko388@gmail.com>
2025-03-08 20:36:09 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,23 @@
+document.querySelector('form').addEventListener('submit', function(event){
+    event.preventDefault();
+
+    let email = document.querySelector('input');
+    let emailerror = document.getElementById('p');
+    let emailerrors = document.getElementById('img');
+    let emailinput = email.value.trim();
+    let emailbtn = document.querySelector('button')
+    let emailpattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    if(emailpattern.test(emailinput)){
+        emailerror.style.display = 'none'
+        emailerrors.style.display = 'none'
+        alert('Valid Email');
+        window.location.href = 'style.css'
+    }
+    else{
+        emailerror.style.display = 'block';
+        emailerrors.style.display = 'block';
+        emailbtn.style.top = '-78px'
+        email.style.borderColor  = 'hsl(0, 93%, 68%)';
+    }
+});
